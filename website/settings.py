507847ac7acc7c599cf654b35f54eb7962d6a7d1
naagh <83181061+naagh@users.noreply.github.com>
2021-12-10 07:52:52 +0000

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('VIRGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['glion.pythonanywhere.com']
 
 
 # Application definition
